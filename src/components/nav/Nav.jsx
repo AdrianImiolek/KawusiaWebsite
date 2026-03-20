4009a80d477dfc501dc0e9e360e889e0769c2e1e
Adrian Imiolek <adrian.imiolek99@gmail.com>
2026-03-20 06:35:55 +0100

    Fixed nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [isHamburgerActive, setIsHamburgerActive] = useState(false);
 
   const links = [
     { name: "O nas", link: "#about" },
